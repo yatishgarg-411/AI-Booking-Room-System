e0d96177f54666ced697b64198cd4ec98d430c00
yatishgarg-411 <126184526+yatishgarg-411@users.noreply.github.com>
2025-07-22 14:35:13 +0530

    CORS updated

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://hotel-booking-room-system-1.onrender.com"],  # React frontend
+    allow_origins=["http://localhost:3000","hotel-booking-room-system.vercel.app"],  # React frontend
     allow_credentials=True,
     allow_methods=["*"],  # GET, POST, PUT, DELETE, etc.
     allow_headers=["*"],  # Authorization, Content-Type, etc.
